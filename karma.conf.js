e83897bd04d5d933ae502a0cded4a728650c0dcd
Uri Shaked <uri@salsa4fun.co.il>
2013-07-03 00:54:44 +0300

    Upgrade to karma 0.9.4

--- karma.conf.js
+++ karma.conf.js
@@ -2,22 +2,21 @@
  * Copyright (C) 2013, Uri Shaked.
  */
 
-/*global JASMINE, JASMINE_ADAPTER, LOG_INFO, files:true, logLevel:true, browsers:true, singleRun:true */
+'use strict';
 
-basePath = '';
-
-files = [
-	JASMINE,
-	JASMINE_ADAPTER,
-	'components/angular/angular.js',
-	'components/angular-mocks/angular-mocks.js',
-	'components/spin.js/dist/spin.js',
-	'angular-spinner.js',
-	'tests.js'
-];
-
-logLevel = LOG_INFO;
-
-browsers = ['PhantomJS'];
-
-singleRun = true;
+module.exports = function (config) {
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine'],
+		logLevel: config.LOG_INFO,
+		browsers: ['PhantomJS'],
+		singleRun: true,
+		files: [
+			'components/angular/angular.js',
+			'components/angular-mocks/angular-mocks.js',
+			'components/spin.js/dist/spin.js',
+			'angular-spinner.js',
+			'tests.js'
+		]
+	});
+};
