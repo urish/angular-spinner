56cd88e7c3b2e8895e55b37b33db94786b343c15
Uri Shaked <uri@salsa4fun.co.il>
2013-08-27 20:41:21 -0300

    Added test coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,18 @@ module.exports = function (config) {
 		frameworks: ['jasmine'],
 		logLevel: config.LOG_INFO,
 		browsers: ['PhantomJS'],
-		singleRun: true,
+		autoWatch: true,
+		reporters: ['dots', 'coverage'],
 		files: [
 			'components/angular/angular.js',
 			'components/angular-mocks/angular-mocks.js',
 			'components/spin.js/dist/spin.js',
 			'angular-spinner.js',
 			'tests.js'
-		]
+		],
+		preprocessors: {
+			'components/spin.js/dist/spin.js': 'coverage',
+			'angular-spinner.js': 'coverage'
+		}
 	});
 };
