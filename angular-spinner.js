1f92596a61664437ab212737f4f5ab1e8fd5d298
tepez <tomyam1@gmail.com>
2013-10-13 13:43:00 +0200

    Update angular-spinner.js
    - Add support for configuration to allow something likw `<span us-spinner="small"></span>`
    - Add an option to make the `<span us-spinner></span>` element large enough to fit the spinner (to make sure it won't be on top of other elements)
    - Use an isolated scope for spinner directive.
    - Save spinner in a local variable instead on the scope.

--- angular-spinner.js
+++ angular-spinner.js
@@ -2,32 +2,85 @@
  * License: MIT.
  * Copyright (C) 2013, Uri Shaked.
  */
+/*global angular*/
 
-'use strict';
 
 angular.module('angularSpinner', [])
-	.directive('usSpinner', ['$window', function ($window) {
-		return {
-			scope: true,
-			link: function (scope, element, attr) {
-				scope.spinner = null;
-				
-				function stopSpinner() {
-					if (scope.spinner) {
-						scope.spinner.stop();
-						scope.spinner = null;
-					}
-				}
-				
-				scope.$watch(attr.usSpinner, function (options) {
-					stopSpinner();
-					scope.spinner = new $window.Spinner(options);
-					scope.spinner.spin(element[0]);
-				}, true);
-				
-				scope.$on('$destroy', function () {
-					stopSpinner();
-				});
-			}
-		};
-	}]);
+
+.provider('usSpinnerConf', [function() {
+    'use strict';
+
+    /**
+     *  This settings will be passed to the Spinner() function.
+     *  <span us-spinner></span> will use the 'default' setting
+     *  <span us-spinner="small"></span> will use the 'small' setting
+     *  <span us-spinner="{ lines: 8}"></span> will use { lines: 8} as a setting
+     *  If makeRoom options is true, will make sure the span element is big  to fit the spinner
+     *  (height-wise and width-wise)
+     */
+    var options = {
+        'default' :{ lines: 8, length: 4, width: 3, radius: 5, usMakeRoom : true },
+        tiny: { lines: 8, length: 2, width: 2, radius: 3, usMakeRoom : true },
+        small: { lines: 8, length: 4, width: 3, radius: 5, usMakeRoom : true },
+        large: { lines: 10, length: 8, width: 4, radius: 8, usMakeRoom : true }
+    };
+
+    this.$get = function $getFn() {
+        return options;
+    };
+
+}])
+
+.directive('usSpinner', ['$window', 'usSpinnerConf', function ($window, usSpinnerConf) {
+    'use strict';
+    return {
+        scope: {
+            options : '@usSpinner'
+        },
+        link: function (scope, element, attr) {
+
+            var spinner = null;
+
+            function stopSpinner() {
+                if (spinner) {
+                    spinner.stop();
+                    spinner = null;
+                }
+            }
+
+            scope.$watch('options', function (options) {
+
+                if (!options) {
+                    options = usSpinnerConf['default'] || {};
+                } else if (angular.isString(options)) {
+                    options = usSpinnerConf[options] || {};
+                }
+
+                stopSpinner();
+                spinner = new $window.Spinner(options);
+
+                // make sure the element the spinner is targeted to is big enough to fit the spinner
+                if (options.usMakeRoom) {
+                    var diameter = spinner.opts.radius * 2 +
+                        spinner.opts.length * 2 +
+                        spinner.opts.width * 2;
+
+                    if (element.width() < diameter) {
+                        element.width(diameter);
+                    }
+                    if (element.height() < diameter) {
+                        element.height(diameter);
+                    }
+
+                    element.css('display', 'inline-block');
+                }
+
+                spinner.spin(element[0]);
+            }, true);
+
+            scope.$on('$destroy', function () {
+                stopSpinner();
+            });
+        }
+    };
+}]);
