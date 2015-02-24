c5b4ff93b5a0fed3241b83c4e2ced735bf2095e6
Ryan Chen <ryan.chen@turn.com>
2015-02-24 13:16:00 -0800

    Fix when using dynamic spinner key
    Signed-off-by: Ryan Chen <ryan.chen@turn.com>

--- angular-spinner.js
+++ angular-spinner.js
@@ -49,8 +49,9 @@
 
 						scope.spinner = null;
 
-						scope.key = angular.isDefined(attr.spinnerKey) ? attr.spinnerKey : false;
-
+						scope.key = scope.$eval(attr.spinnerKey) ? 
+							scope.$eval(attr.spinnerKey) : attr.spinnerKey;
+							
 						scope.startActive = angular.isDefined(attr.spinnerStartActive) ?
 							scope.$eval(attr.spinnerStartActive) : scope.key ?
 							false : true;
